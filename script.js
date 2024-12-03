f7150ef7f457c3696615e5d98adf0c336b19085a
Nimanita <45235857+Nimanita@users.noreply.github.com>
2024-12-03 18:52:50 +0530

    add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const rows = document.querySelectorAll('.row-container:not(.header-container)');
+const contents = document.querySelectorAll('.row-content');
+const radios = document.querySelectorAll('input[type="radio"]:not(.radiobtnforpositioning)');
+
+function toggleRow(index) {
+    // Remove active state from all rows
+    rows.forEach(row => {
+        row.classList.remove('selected');
+    });
+    contents.forEach((content, i) => {
+        content.classList.remove('active');
+        radios[i].checked = false;
+    });
+    console.log(index)
+    // Add active state to selected row
+    rows[index].classList.add('selected');
+    contents[index].classList.add('active');
+    radios[index].checked = true;
+}
